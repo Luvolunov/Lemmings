53ea22884032ac8c24360e986acc4523b7be2393
Vladimir Ivanov <luvolunov@gmail.com>
2022-10-04 15:48:46 +0900

    Initial Commit

---
+++ Lemmings/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lemmings
+//
+//  Created by Vladimir Ivanov on 04.10.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
